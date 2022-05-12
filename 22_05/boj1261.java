38b30c6c0b1646745e11c68d076095b687ac7dd2
이현일 <dpsel57@gmail.com>
2022-05-12 22:50:07 +0900

    0512 algo

---
+++ 22_05/boj1261.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+public class boj1261 {
+	static int row,col,cnt;
+	static char comp[][];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stz = new StringTokenizer(br.readLine());
+		col = Integer.parseInt(stz.nextToken());
+		row = Integer.parseInt(stz.nextToken());
+		comp = new char[row][col];
+		
+		for (int i = 0; i < row; i++) comp[i] = br.readLine().toCharArray();
+		
+		bfs();
+		System.out.println(cnt);
+		
+	}
+	static int[][] dxdy = { { 0, 0, 1, -1 }, { 1, -1, 0, 0 } };
+	private static void bfs() {
+		PriorityQueue<int[]> q = new PriorityQueue<>((o1,o2)->o1[2]-o2[2]);
+		q.offer(new int[] {0,0,0});//x y
+		int temp[],nx,ny,val;
+		boolean checked[][] = new boolean[row][col];
+		checked[0][0] = true;
+		while(!q.isEmpty()) {
+			temp = q.poll();
+			
+			if(temp[0]==row-1 && col-1==temp[1]) {
+				cnt = temp[2]; 
+				return;
+			}
+			for(int i=0;i<4;i++) {
+				nx = temp[0]+dxdy[0][i];
+				ny = temp[1]+dxdy[1][i];
+				
+				if(0<=nx && nx<row && 0<=ny && ny<col && !checked[nx][ny]) {
+					val = temp[2];
+					if(comp[nx][ny]=='1') {
+						val += 1;
+					}
+					checked[nx][ny] = true;
+					q.offer(new int[] {nx,ny,val});
+				}
+			}
+		}
+	}
+}
